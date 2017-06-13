b785c44c8b99fcad12d712d04ab89d2b9d1b4b7a
odarlie <odarlie@gmail.com>
2017-06-13 11:44:36 +0300

    This is the code for the fizzbuzz function

--- fizz_buzz_bootcamp_function.py
+++ fizz_buzz_bootcamp_function.py
@@ -1,6 +1,6 @@
 def fizz_buzz(number):
     ''' This function returns 'Fizz,'Buzz,FizzBuzz' or the argument it recieves based
-on the argument the function recieves. '''
+on the argument the function recieves.. '''
     if number %5 ==0 and number %3 ==0:
         return 'FizzBuzz'
     else:
