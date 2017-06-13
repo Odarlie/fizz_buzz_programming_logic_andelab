0e7555e9beff8ec6daa75e4e4766229c66d3a2aa
odarlie <odarlie@gmail.com>
2017-06-13 11:33:30 +0300

    This is     ''' This function returns 'Fizz,'Buzz,FizzBuzz' or the argument it recieves based

---
+++ fizz_buzz_bootcamp_function.py
@@ -0,0 +1,21 @@
+def fizz_buzz(number):
+    ''' This function returns 'Fizz,'Buzz,FizzBuzz' or the argument it recieves based
+on the argument the function recieves. '''
+    if number %5 ==0 and number %3 ==0:
+        return 'FizzBuzz'
+    else:
+        if number % 3 == 0:
+            return 'Fizz'
+        elif number % 5 == 0:
+            return 'Buzz'
+        else:
+            return number
+            
+    
+        
+    
+        
+  
+    
+
+    
